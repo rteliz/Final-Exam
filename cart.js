380dcf06e4bc07d39cf98d0545967051ca571c2f
rteliz <9rteliz@stpatrick.org>
2016-12-15 15:21:14 -0600

    day1

---
+++ cart.js
@@ -0,0 +1,9 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+
+
+
+  //Type the instance functions below this comment.
+
+
+}
---
+++ item.js
@@ -0,0 +1,8 @@
+class Item{
+  //Fill in the item class below this comment.
+
+
+
+}
+
+//Create your three test items below this comment.
